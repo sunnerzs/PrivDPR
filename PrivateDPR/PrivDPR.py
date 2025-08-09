733d31afeaaf3e39ddc06df9d4c9d7adb3110c56
sunnerzs <zhangsen98k@163.com>
2025-08-09 13:45:58 +0800

    Update PrivDPR.py

--- PrivateDPR/PrivDPR.py
+++ PrivateDPR/PrivDPR.py
@@ -125,11 +125,11 @@ class DiGraSynModel:
                             is many orders of magnitude larger than the true gradient. This leads 
                             to difficulty in maintaining the number of triangles in the generated synthetic 
                             graph. Indeed, the results of TC in Tables 2 and 3 formally confirm this point in the paper, 
-                            while the distribution information is relatively well maintained. One important reason for this situation 
-                            is that a one-hot encoding was applied before the input embedding matrix, which may result in many 
-                            zero gradients in the gradient with respect to the input matrix. Since gradient information is not 
-                            released and only the synthetic graph is published, noise can be added only to the non-zero gradients 
-                            in the future, which helps improve the situation.
+                            while the distribution information is relatively well maintained. One important reason 
+                            for this situation is that a one-hot encoding was applied before the input embedding matrix, 
+                            which may result in many zero gradients in the gradient with respect to the input matrix. Since 
+                            gradient information is not released and only the synthetic graph is published, noise can be added 
+                            only to the non-zero gradients in the future, which helps improve the situation.
                             '''
                             noise_g = g + self.Gau_Noise(g, args.g_clip)
                             self.grads_and_vars[i] = (noise_g, v)
